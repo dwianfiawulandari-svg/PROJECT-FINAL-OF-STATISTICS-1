15c7bdf17cfbf64d838416797f13e9490e239b30
dwianfiawulandari-svg <dwianfia.wulandari@student.president.ac.id>
2025-12-04 11:43:14 +0700

    Initialize Streamlit app for Matrix Transform

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Matrix Transform App",
+    page_icon="📘",
+)
+
+st.title("Aplikasi Transformasi Matriks")
+st.write("Selamat datang! Silakan pilih halaman dari sidebar 😊")
